cf93dccd360f7168f0f68348d5733b9f5fe70180
Aritra <chatterjee.aritra@protonmail.com>
2024-02-25 12:16:29 +0530

    adding routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) =>{
+  console.log("Signup User")
+}
+
+export const login = (req, res) =>{
+  console.log("Login user")
+}
+
+export const logout = (req, res) =>{
+  console.log("Logout User")
+}
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 dotenv.config()
@@ -10,5 +10,6 @@ app.get("/", (req, res) =>{
   res.send("Server is ready")
 })
 
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
